be6ea20d7df24c0f014183003530d79abf875ea8
haradeva <haradev555@gmail.com>
2025-02-06 11:27:40 +0530

    Update dependencies and fix Navbar component

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -355,7 +355,7 @@ export default function Navigation() {
 
               {/* Logo */}
               <div className="ml-4 flex lg:ml-0">
-                <a href="#">
+                <a href="/">
                   <span className="sr-only">LS Collections</span>
                   <img
                     alt="LSCollections"
